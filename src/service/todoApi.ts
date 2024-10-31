7a52f2b967555b0f02e0cb730c2ffae80964fa69
hozayves <muhozayves2014@gmail.com>
2024-10-31 13:15:08 +0200

    Finish todo api services

---
+++ src/service/todoApi.ts
@@ -0,0 +1,36 @@
+import axios from "axios"
+import { ApiTodo, FormattedTodo } from "../types/todo"
+import { getRandomStatus } from "../utils/status"
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+export const todoApi = {
+  getTodos: async () => {
+    const { data } = await axios.get<{ todos: ApiTodo[] }>(`${API_BASE_URL}/todos`)
+    return data.todos.map((item: ApiTodo) => ({
+      id: item.id,
+      title: item.todo,
+      description: "Landing page UI",
+      status: getRandomStatus(),
+      messageCount: Math.floor(Math.random() * 15),
+      assignedUser: Math.floor(Math.random() * 2) + 2,
+    }))
+  },
+
+  deleteTodo: async (id: number) => {
+    const { data } = await axios.delete<ApiTodo & { isDeleted: boolean; deletedOn: string }>(
+      `${API_BASE_URL}/todos/${id}`
+    )
+    return data
+  },
+
+  updateTodo: async (id: number, updatedTodo: Partial<FormattedTodo>) => {
+    const { data } = await axios.put(`${API_BASE_URL}/todos/${id}`, updatedTodo)
+    return data
+  },
+
+  createTodo: async (newTodo: { todo: string; userId: number; completed: boolean }) => {
+    const { data } = await axios.post(`${API_BASE_URL}/todos/add`, newTodo)
+    return data
+  },
+}
